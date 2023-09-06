50e80be99918c834a1e39f34f656a7da591fee02
UsmanSaeed787 <usmanalisaeed787@gmail.com>
2023-09-06 15:54:14 +0500

    Variable practice

---
+++ index7.js
@@ -0,0 +1,58 @@
+"use strict";
+//QUESTION 2
+// Personal Message: Store a person’s name in a variable, and print a message to 
+// that person. Your message should be simple, such as, “Hello Eric, would you like 
+// to learn some Python today?”
+var a1 = "Eric";
+console.log("Hello " + a1 + " Would you like to learn type script today");
+// QUESTION 3
+// Name Cases: Store a person’s name in a variable, and then print that person’s 
+// name in lowercase, uppercase, and titlecase.
+var person_name = "Usman Saeed";
+console.log(person_name.toLowerCase());
+console.log(person_name.toUpperCase());
+// Question 4
+// Famous Quote: Find a quote from a famous person you admire. Print the 
+// quote and the name of its author. Your output should look something 
+// like the following, including the quotation marks:
+const quote1 = "When You Succeed Be Humble";
+const quote2 = "Imam Ali [as]";
+console.log(quote1 + ":", quote2);
+//Question 5
+const quote3 = quote2;
+console.log(quote1 + ":", quote3);
+// Question 6
+// Stripping Names: Store a person’s name, and include 
+// some whitespace characters at the beginning and end of the name. 
+// Make sure you use each character combination, "\t" and "\n", at least once. 
+// Print the name once, so the whitespace around the name is displayed. 
+// Then print the name after striping the white spaces.
+var stripName = "Usm \t an \n Saeed";
+console.log(stripName);
+//Question 7 & 8
+// Number Eight: Write addition, subtraction, multiplication, and division operations
+// that each result in the number 8. Be sure to enclose your operations in print 
+// statements to see the results.
+var a = 1;
+var b = 2;
+var c = 3;
+var d = 4;
+var e = 12;
+var f = 16;
+console.log(d + d);
+console.log(e - d);
+console.log(b * d);
+console.log(f / b);
+// Question 9
+// Favorite Number: Store your favorite number in a variable. Then, using that 
+// variable, create a message that reveals your favorite number. Print that message.
+let favNum = 7;
+console.log("MY FAVOURITE NUMBER IS : ", favNum);
+// QUESTION 10
+// Adding Comments: Choose two of the programs you’ve written, and add at least one
+// comment to each. If you don’t have anything specific to write because your
+// programs are too simple at this point, just add your name and the current date
+// at the top of each program file. Then write one sentence describing what the
+// program does.
+/*HELLO MY NAME IS USMAN SAEED, TODAY DATE IS 09/06/023, I AM STUDENT OF PIAIC AND LEARNING TYPE SCRIPT*/
+//MY AIM IS TO BECOME IT EXPERT.
